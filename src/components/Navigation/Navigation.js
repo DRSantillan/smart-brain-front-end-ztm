22f1816f113f3dccf31fed4a107c10d1fadb73c2
Dean <dean@deansantillan.com>
2021-12-23 11:12:48 +1000

    task(): added background, style sheet for navigation,jsx for navigation, installed tachyons

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,8 +1,14 @@
 import React from 'react';
+
 import './Navigation.css';
 
+// Simple Component > No state (Pure Function)
 const Navigation = () => {
-	return <div>Navigation</div>;
+	return (
+		<nav className='navigation'>
+			<p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+		</nav>
+	);
 };
 
 export default Navigation;
