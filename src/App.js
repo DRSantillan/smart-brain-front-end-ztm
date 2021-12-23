c3e2026844a49ffd3eb6e1e7598c6f3ed028fcc5
Dean <dean@deansantillan.com>
2021-12-23 12:53:09 +1000

    task(): installed the react-particles-js but didnt work cause it keeps asking for tsparticles.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
-
+//import Particles from 'react-particles-js';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import './App.css';
-
+const particlesOptions = {
+	particles: {
+		number: {
+			value: 50,
+			density: {
+				enable: true,
+				value_area: 800,
+			},
+		},
+		opacity: {
+			value: 0.5,
+		},
+		shape: {
+			type: 'circle',
+		},
+		size: {
+			random: true,
+			value: 5,
+		},
+	},
+};
 class App extends Component {
 	render() {
 		return (
 			<div className='App'>
+				<Particles className='particles' params={particlesOptions} />
 				<Navigation />
 				<Logo />
 				<Rank />
