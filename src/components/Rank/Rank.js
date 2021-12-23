8cf2f5ff9e3e97d0462c208f25ec2a9f3eb7afcf
Dean <dean@deansantillan.com>
2021-12-23 11:55:47 +1000

    task(): completed ImageLInkForm visuals, completed Rank visuals

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -2,7 +2,12 @@ import React from 'react';
 import './Rank.css';
 
 const Rank = () => {
-	return <div>Rank</div>;
+	return (
+		<div>
+			<div className='white f3'>{'Dean, your current rank is....'}</div>
+			<div className='white f1'>{'#5'}</div>
+		</div>
+	);
 };
 
 export default Rank;
